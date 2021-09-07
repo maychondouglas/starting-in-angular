c93bdffd0b1da3751e24343c03bf5848f05e24f1
maychondouglas <maychondouglas@gmail.com>
2021-09-07 20:48:44 -0300

    Paginator and Sort builded

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -1,6 +1,6 @@
 import { ProductService } from './../product.service';
 import { Product } from './../product.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-read',
@@ -9,15 +9,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductReadComponent implements OnInit {
 
-  products!: Product[]; 
+  @Input()
+  listaDeProdutos!: Product[];
+
+  dataSource! : Product[];
+  pageSize = 20;
+  pageArray = Array();
+  pageIterador = 0;
+  currentPage!: string;
+
   displayedColumns = ['id', 'name', 'price', 'action'];
 
-  constructor(private productService: ProductService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.productService.read().subscribe(products => {
-      this.products = products;
-    })
+    //this.dataSource = this.listaDeProdutos;
+    this.pageSize = (this.listaDeProdutos.length < 20)? this.listaDeProdutos.length : 20;
+    this.calculatePageArray();
+    this.goToFirstPage();
   }
 
+  //Retorna para a página anterior, caso não seja a primeira página
+  getPrevPage(): void {
+    if(this.pageIterador > 0) {
+      this.pageIterador --;
+      this.dataSource = this.listaDeProdutos.slice(
+        this.pageArray[this.pageIterador].start,
+        this.pageArray[this.pageIterador].end
+      )
+      this.currentPage = `${this.pageArray[this.pageIterador].start + 1} - ${this.pageArray[this.pageIterador].end} de ${this.listaDeProdutos.length}`;
+    }
+  }
+
+  //Avança a página caso não seja a última página
+  getNextPage(): void {
+    if(this.pageIterador < this.pageArray.length - 1) {
+      this.pageIterador ++;
+      this.dataSource = this.listaDeProdutos.slice(
+        this.pageArray[this.pageIterador].start,
+        this.pageArray[this.pageIterador].end
+      );
+
+      this.currentPage = `${this.pageArray[this.pageIterador].start + 1} - ${this.pageArray[this.pageIterador].end} de ${this.listaDeProdutos.length}`;
+    }
+    
+  }
+
+  //Monta um Array de objetos para guardar o início e fim de cada página
+  calculatePageArray(): void {
+    for(let i = 0; i < this.listaDeProdutos.length; i += this.pageSize) {
+      this.pageArray.push({start: i, end: (i + this.pageSize <= this.listaDeProdutos.length)? i + this.pageSize : this.listaDeProdutos.length});
+    }
+  }
+
+  //Ordena o Array
+  sortData(direction: 'asc' | 'desc'): void {
+    if(direction === 'asc') {
+      this.listaDeProdutos = this.listaDeProdutos.sort(
+        (a : any, b: any) => (a.id < b.id) ? -1 : 1 );
+    } else {
+      this.listaDeProdutos = this.listaDeProdutos.sort( 
+        (a : any, b: any) => (a.id > b.id) ? -1 : 1 );
+    }
+
+    this.goToFirstPage();
+  }
+
+  goToFirstPage() : void {
+    this.pageIterador = 0;
+      this.dataSource = this.listaDeProdutos.slice(
+        this.pageArray[this.pageIterador].start,
+        this.pageArray[this.pageIterador].end
+      );
+
+      this.currentPage = `${this.pageArray[this.pageIterador].start + 1} - ${this.pageArray[this.pageIterador].end} de ${this.listaDeProdutos.length}`;
+  }
 }
--- src/app/views/product-crud/product-crud.component.ts
+++ src/app/views/product-crud/product-crud.component.ts
@@ -1,6 +1,9 @@
+import { ProductService } from './../../components/product/product.service';
 import { HeaderService } from './../../components/template/header/header.service';
 import { Component, OnInit } from '@angular/core';
 import { Router} from '@angular/router';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/components/product/product.model';
 
 @Component({
   selector: 'app-product-crud',
@@ -9,9 +12,12 @@ import { Router} from '@angular/router';
 })
 export class ProductCrudComponent implements OnInit {
 
+  produtosObtidos!: Product[];
+
   constructor(
     private router: Router,
-    private headerService: HeaderService
+    private headerService: HeaderService,
+    private productService: ProductService
   ) { 
     this.headerService.headerData = {
       title: 'Cadastro de Produtos',
@@ -21,10 +27,19 @@ export class ProductCrudComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getProductsList();
   }
 
   navigateToProductCreate(): void {
     this.router.navigate(['products/create']);
+  }
+
+  getProductsList(): void {
+    this.productService.read().subscribe(products => {
+      this.produtosObtidos = products;
+      console.log(this.produtosObtidos)
+      return;
+    })
   }
 
 }
